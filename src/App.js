9ab3f47811409e699b5bdc5bccf09ab0615bcf56
Charlie025x <charliealonso2001@gmail.com>
2022-05-12 09:56:09 -0500

    ran create-react-app .

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Features Company Careers About Login Register Make remote work Get your
+      team in sync, no matter your location. Streamline processes, create team
+      rituals, and watch productivity soar. Learn more
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a
+          href="https://charlie-alonso.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Charlie Alonso
+        </a>
+        .
+      </div>
+    </div>
+  );
+}
+
+export default App;
